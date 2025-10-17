2a113a7a4b3201771b542d90c753c119ec5f8106
@tuanhq <quoctuan200702@gmail.com>
2025-10-18 00:14:16 +0700

    feat: introduce new mobile template application with initial setup, including navigation, theming, and example components, to enhance mobile development experience and provide a foundation for future features

--- packages/env/src/index.js
+++ packages/env/src/index.js
@@ -5,14 +5,14 @@ exports.env = void 0;
 const env_nextjs_1 = require("@t3-oss/env-nextjs");
 const zod_1 = require("zod");
 const env = (0, env_nextjs_1.createEnv)({
-    server: {},
-    client: {
-        NEXT_PUBLIC_ENV: zod_1.z.string().optional(),
-        NEXT_PUBLIC_PORTFOLIO_DOMAIN: zod_1.z.string().url(),
-    },
-    experimental__runtimeEnv: {
-        NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
-        NEXT_PUBLIC_PORTFOLIO_DOMAIN: process.env.NEXT_PUBLIC_PORTFOLIO_DOMAIN,
-    },
+  server: {},
+  client: {
+    NEXT_PUBLIC_ENV: zod_1.z.string().optional(),
+    NEXT_PUBLIC_PORTFOLIO_DOMAIN: zod_1.z.string().url(),
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
+    NEXT_PUBLIC_PORTFOLIO_DOMAIN: process.env.NEXT_PUBLIC_PORTFOLIO_DOMAIN,
+  },
 });
 exports.env = env;
--- toolings/eslint/base.js
+++ toolings/eslint/base.js
@@ -1,9 +1,9 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import turboPlugin from "eslint-plugin-turbo";
 import unusedImports from "eslint-plugin-unused-imports";
+import tseslint from "typescript-eslint";
 
 /**
  * A shared ESLint configuration for the repository.
