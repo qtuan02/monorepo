195ea984639503641a58c56bce90c77d8cdce335
@tuanhq <quoctuan200702@gmail.com>
2025-10-20 22:39:59 +0700

    feat: add Google Analytics and Tag Manager environment variables to configuration and update layout to utilize them

--- packages/env/src/index.d.ts
+++ packages/env/src/index.d.ts
@@ -1,5 +1,7 @@
 declare const env: Readonly<{
     NEXT_PUBLIC_ENV?: string | undefined;
+    NEXT_PUBLIC_GOOGLE_ANALYTICS?: string | undefined;
+    NEXT_PUBLIC_GOOGLE_TAG_MANAGER?: string | undefined;
     NEXT_PUBLIC_PORTFOLIO_DOMAIN: string;
 }>;
 export { env };
--- packages/env/src/index.js
+++ packages/env/src/index.js
@@ -9,10 +9,14 @@ const env = (0, env_nextjs_1.createEnv)({
     client: {
         NEXT_PUBLIC_ENV: zod_1.z.string().optional(),
         NEXT_PUBLIC_PORTFOLIO_DOMAIN: zod_1.z.string().url(),
+        NEXT_PUBLIC_GOOGLE_ANALYTICS: zod_1.z.string().optional(),
+        NEXT_PUBLIC_GOOGLE_TAG_MANAGER: zod_1.z.string().optional(),
     },
     experimental__runtimeEnv: {
         NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
         NEXT_PUBLIC_PORTFOLIO_DOMAIN: process.env.NEXT_PUBLIC_PORTFOLIO_DOMAIN,
+        NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+        NEXT_PUBLIC_GOOGLE_TAG_MANAGER: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER,
     },
 });
 exports.env = env;
